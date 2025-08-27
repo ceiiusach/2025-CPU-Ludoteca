ce5a84628af4eb543dbf5fdbede1c04fe071e6ab
cpinid <cpinid@cpini.dev>
2025-08-27 00:51:08 -0400

    (front) Bosquejo de página principal y enrutador

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,6 +12,9 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  server:{
+    allowedHosts: true
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
